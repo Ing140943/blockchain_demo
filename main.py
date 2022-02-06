ee3df07c008ed070a5611b06a17e84d1efe1ac23
Ing140943 <skladeeing@gmail.com>
2022-02-06 17:08:36 +0700

    Create api via flask

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from blockchain import Blockchain
+from flask import Flask
+
+# web server
+app = Flask(__name__)
+blockchain = Blockchain() # create genesis block
+#routing
+@app.route('/')
+def hello():
+    return "<h1>Hello Blockchain</h1>"
 
 if __name__ == "__main__":
-    blockchain = Blockchain() # create genesis block
-    print(blockchain.chain[0]) # see the genesis block
-    print(blockchain.hash(blockchain.chain[0])) # encode the block
+    app.run()
+    # print(blockchain.chain[0]) # see the genesis block
+    # print(blockchain.hash(blockchain.chain[0])) # encode the block
